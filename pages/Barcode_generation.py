d84821ff9bbff3065e8740613fcee1165b849e63
Hasoor-A-Bhatti <hb01395@surrey.ac.uk>
2024-02-09 18:00:48 +0000

    barcode generation v1

--- pages/Barcode_generation.py
+++ pages/Barcode_generation.py
@@ -7,18 +7,24 @@ import io
 def generate_barcode(input_data):
     # Generate barcode
     barcode = Code128(input_data, writer=ImageWriter())
+
+    # Create a BytesIO object to hold the image data
     img_bytes = io.BytesIO()
+
+    # Save the barcode image to the BytesIO object
     barcode.write(img_bytes)
     img_bytes.seek(0)
+
     # Open the image using PIL
     img = Image.open(img_bytes)
     return img
 
 # Streamlit UI
 st.title('Barcode Generator')
-input_data = st.text_input('Enter Product Name')
+input_data = st.text_input('Enter data for barcode generation:')
 if st.button('Generate Barcode'):
     if input_data:
         barcode_image = generate_barcode(input_data)
+        st.image(barcode_image, caption=f'Barcode for {input_data}', use_column_width=True)
     else:
-        st.warning('No product name entered.')
+        st.warning('Please enter some data to generate a barcode.')
